ec5613d5c9f34c6afe7f16a7c9436f181c682273
Chris Padilla <cdpadilla42@gmail.com>
2020-06-19 14:13:19 -0500

    Sketch picross game

---
+++ picross.js
@@ -0,0 +1,32 @@
+function Picross(grid) {
+  const squares = Array.from(grid.querySelectorAll('.square'));
+
+  function fillSquare(square) {
+    square.classList.add('selected');
+  }
+
+  function unFillSquare(square) {
+    square.classList.remove('selected');
+  }
+
+  function handleKeyUp(e) {
+    console.log('pressed!');
+    if (e.key === 'Enter') handleSquareClick(e.currentTarget);
+  }
+
+  function handleSquareClick(s) {
+    if (s.matches('.selected')) return unFillSquare(s);
+    fillSquare(s);
+  }
+
+  squares.forEach((s) => {
+    s.addEventListener('click', () => handleSquareClick(s));
+  });
+
+  squares.forEach((s) => {
+    s.addEventListener('keyup', (e) => handleKeyUp(e));
+  });
+}
+
+const picross1 = Picross(document.querySelector('.picross1'));
+const picross2 = Picross(document.querySelector('.picross2'));
