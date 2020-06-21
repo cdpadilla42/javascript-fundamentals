2ad93b78bce67e5c6ddb2f354fc9a59a5e8d100d
Chris Padilla <cdpadilla42@gmail.com>
2020-06-21 15:18:19 -0500

    Add close method to modal with key bindings

--- picross.js
+++ picross.js
@@ -1,5 +1,6 @@
 function Picross(grid) {
-  console.log(grid);
+  let selectedSquares = new Array(25).fill(false, 0, 25);
+  console.log(selectedSquares);
   const squares = Array.from(grid.querySelectorAll('.grid-container .square'));
   const modal = document.querySelector('.modal');
   const button = grid.querySelector('button');
@@ -9,10 +10,33 @@ function Picross(grid) {
       return;
     }
     modal.classList.remove('hide');
+
+    // Modal Event Binding
+    modal.addEventListener('click', handleClickOutside);
+    window.addEventListener('keyup', handleKeyUp);
+    modal
+      .querySelector('.inner_modal span')
+      .addEventListener('click', closeModal);
+  }
+
+  function closeModal() {
+    modal.classList.add('hide');
+
+    // Modal Event Un-binding
+    modal.removeEventListener('click', handleClickOutside);
+    window.removeEventListener('keyup', handleKeyUp);
+    modal
+      .querySelector('.inner_modal span')
+      .removeEventListener('click', closeModal);
+  }
+
+  function handleClickOutside(e) {
+    if (e.target == e.currentTarget) closeModal();
   }
 
   function fillSquare(square) {
     square.classList.add('selected');
+    console.log(square.dataset.sq);
   }
 
   function unFillSquare(square) {
@@ -21,6 +45,7 @@ function Picross(grid) {
 
   function handleKeyUp(e) {
     if (e.key === 'Enter') handleSquareClick(e.currentTarget);
+    if (e.key === 'Escape') closeModal();
   }
 
   function handleSquareClick(s) {
